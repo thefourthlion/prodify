cb97f3118db1aa4519ca7dfe6c98f2446352db58
everettthefourth <the00110100lion@gmail.coma>
2024-03-25 05:54:00 -0700

    mvp of dailycheck

--- client/react/src/components/DailyCheck.js
+++ client/react/src/components/DailyCheck.js
@@ -1,23 +1,56 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import getDate from "../services/date";
+import getTimestamp from "../services/timestamp";
+import refreshPage from "../services/refresh";
+
 const DailyCheck = () => {
-    const [tasks, setTasks] = useState([]);
-    const [tasksDone, setTasksDone] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [tasksDone, setTasksDone] = useState([]);
+  const [points, setPoints] = useState(0);
 
-    const fetchData = async () => {
-        try {
-          const response = await axios.get("http://localhost:3002/api/tasks/read");
-          setTasks(response.data);
-        } catch (error) {
-          console.error("Error Fetching Data", error);
+  const date = getDate();
+  const timestamp = getTimestamp();
+
+  const createData = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:3002/api/tasks/create",
+        {
+          date: date,
+          tasks: tasksDone,
+          points: points,
+          timestamp: timestamp,
         }
-      };
+      );
+      console.log(response.data);
+      refreshPage();
+    } catch (error) {
+      console.error("Error Posting Data", error);
+    }
+  };
 
-      useEffect(() => {
-        fetchData();
-      }, []);
-    
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3002/api/tasks/read");
+      setTasks(response.data);
+    } catch (error) {
+      console.error("Error Fetching Data", error);
+    }
+  };
+
+  const getTotalPoints = () => {
+    const totalPoints = tasksDone.reduce((acc, currentItem) => {
+      return acc + Number(currentItem.points);
+    }, 0);
+    console.log(totalPoints)
+    setPoints(totalPoints);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="DailyCheck">
       <div className="container">
@@ -26,17 +59,30 @@ const DailyCheck = () => {
         {tasks.map((task) => (
           <div key={task._id}>
             <p>
-
-                <input type="checkbox" onClick={()=>{
-                    setTasksDone([...tasksDone, { task: task.task, points: task.points }])
-                }}/>
+              <input
+                type="checkbox"
+                onClick={() => {
+                  setTasksDone([
+                    ...tasksDone,
+                    { task: task.task, points: task.points },
+                  ]);
+                }}
+              />
               {task.task} | {task.points} points{" "}
-             
             </p>
           </div>
         ))}
 
-<button onClick={(()=>{console.log(tasksDone)})}>log</button>
+        <button
+          onClick={() => {
+            console.log(tasksDone);
+            getTotalPoints()
+          }}
+        >
+          log
+        </button>
+
+        <h1>{points}</h1>
       </div>
     </div>
   );
